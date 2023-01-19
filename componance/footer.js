7bd3451e093a4b41427d31c3b7b56281c5fc7cf1
jaroensakyod <jaroensak.yod@gmail.com>
2023-01-19 23:11:44 +0700

    addcss

--- componance/footer.js
+++ componance/footer.js
@@ -1,7 +1,24 @@
 export default function Footer(){
           return(
                     <>
+                   
+                    <section class="message-list">
+                    <section class="message -left">
+                    
                     <i class="nes-mario"></i>
+                    <div class="nes-balloon from-left">
+                    <p>Hello NES.css</p>
+                    </div>
+                    </section>
+
+                    <section class="message -right">
+                    <div class="nes-balloon from-right">
+                    <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+                    </div>
+                    
+                    <i class="nes-mario"></i>
+                    </section>
+                    </section>
                     </>
           )
 }
